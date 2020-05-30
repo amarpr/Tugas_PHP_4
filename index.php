1bfe287ebbd7f2fca520279e70df293ea632a7f2
Muhamar <amararrasyid@gmail.com>
2020-05-30 20:45:55 +0700

    Tugas_4_PHP
    Mengerjakan Tugas Niomic

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+$beli = 8000;
+$jual = 7500;
+$untung = 300;
+
+echo "Harga Beli = Rp. $beli <br>";
+echo " Harga Jual = Rp. $jual <br>";
+echo "Keuntungan = Rp. $untung <br>";
+echo "Berapa Jumlah Kambing yang dibeli ? <br>";
+echo "Jawaban : "
+?>
---
+++ index2.php
@@ -0,0 +1,10 @@
+<?php
+$angka_pertama = 523;
+$angka_kedua = 8891;
+$hasil = $angka_kedua / $angka_pertama;
+
+echo "Jumlah Saluran = $angka_pertama <br>";
+echo "Jumlah Terpakai = $angka_kedua <br>";
+echo " Rata-rata Pemakaian = ....? <br>";
+echo "Jawaban : $hasil liter/keluarga";
+?>
